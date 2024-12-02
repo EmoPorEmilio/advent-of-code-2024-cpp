15041c7b54e4d9d01470bc042e2d15b29bcb9cde
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-12-02 14:46:01 -0300

    day 2 part 1

---
+++ 02/a.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int calculate_distance(int x1, int x2) {
+    return abs(x1 - x2);
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output_b.txt", "w", stdout);
+    string line;
+    int sum = 0;
+    while (getline(cin, line)) {        
+        istringstream iss(line);
+        int current;
+        int prev = -1;
+        bool valid = true;
+        bool checked = false;
+        bool ascending = true;
+        while (iss >> current && valid) {
+            if (prev != -1) {
+                if (prev == current) {
+                    valid = false;
+                }
+                if (calculate_distance(prev, current) > 3) {
+                    valid = false;
+                }
+                if (!checked) {
+                    if (prev > current) {
+                        ascending = false;
+                    }
+                    else {
+                        ascending = true;
+                    }
+                    checked = true;
+                }
+                else {
+                    if (ascending && prev > current) {
+                        valid = false;
+                    }
+                    if (!ascending && prev < current) {
+                        valid = false;
+                    }
+                }
+                if (!valid) {
+                    break;
+                }
+            }
+            prev = current;
+        }
+        if (valid) {
+            sum += 1;
+        }
+    }
+    printf("%d\n", sum);
+
+    return 0;               
+
+}
